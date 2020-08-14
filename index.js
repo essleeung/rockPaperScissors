2faa2127c15f4b2c2b38379732b6f6fda636e045
essleeung <essleeung@gmail.com>
2020-08-14 12:48:00 -0400

    added most of ui

---
+++ index.js
@@ -0,0 +1,10 @@
+// let rulesButton = document.getElementsByClassName('rules-btn')
+// let rulesPic = document.getElementById('modal')
+// // rules modal
+// function modalOpen() {
+//     rulesPic.setAttribute('class', 'show')
+//     console.log('open modal')
+// }
+// window.addEventListener('DOMContentLoaded', (e) => {
+//     rulesButton.addEventListener('click', modalOpen())
+// });
